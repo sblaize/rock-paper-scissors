189734614bcb9f54e695a63df4976443760d467a
sblaize <blaizesekani@gmail.com>
2023-01-18 17:53:59 -0500

    Added dom elements to button press

--- javascript.js
+++ javascript.js
@@ -10,7 +10,10 @@
     const move1 = move[Math.floor(Math.random()*move.length)];
     return move1
 }
-
+const computerSelection = computerPlay()
+let playerScore = 0;
+let compScore = 0;
+const container = document.querySelector('#container')
 //Player plays computer in Rock, Paper, or Scissors in a single round
 function playRound(playerSelection, computerSelection) {
     let rock = "Rock";
@@ -19,9 +22,9 @@ function playRound(playerSelection, computerSelection) {
     let p = paper.toLowerCase();
     let scissors = "Scissors";
     let s = scissors.toLowerCase();
-    const player = `Player selects: ${playerSelection}. Computer selects ${computerSelection}. Player Wins`
-    const computer = `Player selects: ${playerSelection}. Computer selects ${computerSelection}. Computer Wins`
-    const tie = `Player selects: ${playerSelection}. Computer selects ${computerSelection}. It's a tie!`
+    const player = `Player selects: ${playerSelection}. Computer selects ${computerSelection}. Player Wins. Player Score is ${playerScore + 1}. Computer Score is ${compScore}.`
+    const computer = `Player selects: ${playerSelection}. Computer selects ${computerSelection}. Computer Wins. Player Score is ${playerScore}. Computer Score is ${compScore + 1}.`
+    const tie = `Player selects: ${playerSelection}. Computer selects ${computerSelection}. It's a tie!. Player Score is ${playerScore}. Computer Score is ${compScore}.`
 if (playerSelection === computerSelection) {
 return tie
 } else if (playerSelection === r && computerSelection === s){
@@ -46,39 +49,47 @@ return computer;
 ++compScore
 return computer;
 }
+
+
 }
 
 
 const rockButton = document.getElementById('rock')
 const paperButton = document.getElementById('paper')
 const scissorsButton = document.getElementById('scissors')
-
 rockButton.addEventListener('click', () => {
     const rock = "rock";
     const r = rock.toLowerCase();
     const playerSelection = r 
     const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection))});
+    const div = document.createElement('div');
+    const pR = playRound(playerSelection, computerSelection);
+    div.textContent = pR;
+    container.appendChild(div);
+});
+
 
 paperButton.addEventListener('click', () => {
         const paper = "paper";
         const p = paper.toLowerCase();
         const playerSelection = p
         const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection))});
+        const div = document.createElement('div');
+        const pR = playRound(playerSelection, computerSelection);
+        div.textContent = pR;
+        container.appendChild(div);
+});
         
 scissorsButton.addEventListener('click', () => {
         const scissors = "scissors";
         const s = scissors.toLowerCase();
         const playerSelection = s
         const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection))});
-
-
-
-
-let playerScore = 0;
-let compScore = 0;
+        const div = document.createElement('div');
+        const pR = playRound(playerSelection, computerSelection);
+        div.textContent = pR;
+        container.appendChild(div);
+});
 
 
 
